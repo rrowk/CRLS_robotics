eac9a7f807e87667c553b6506af07b1371808f71
Enator18 <62544405+Enator18@users.noreply.github.com>
2023-12-20 13:50:16 -0500

    added drive thrusters

--- PythonScripts/bot.py
+++ PythonScripts/bot.py
@@ -9,6 +9,8 @@ i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus)
 pca.frequency = 50
 
+drive_speed = 25
+
 
 app = flask.Flask("RobotServer")
 @app.route("/")
@@ -20,20 +22,30 @@ def send():
     os.system('clear')
     all_args = flask.request.args.to_dict()
     time.sleep(0.1)
+    joy1x = 0
+    joy1y = 0
+    joy2y = 0
+
     for arg in all_args:
         #print all the keys then : then the values
         print(arg,":",all_args[arg])
     #must return something, doesn't really matter what
     return all_args
 
+    thrust_left = min(1, max(-1, joy1y + joy1x)) * drive_speed
+    thrust_right = min(1, max(-1, joy1y - joy1x)) * drive_speed
+    thrust_up = joy2y * drive_speed
+
+    set_thruster_speed(0, thrust_left)
+    set_thruster_speed(1, thrust_right)
+    set_thruster_speed(3, thrust_up)
+    set_thruster_speed(4, thrust_up)
+
 #Thruster index 0-3
 #Speed -100 to 100
 def set_thruster_speed(thruster, speed)
 {
-    frequency = 50
-    pulse_length = 1500 + (4 * speed)
-    pca.channels[thruster].duty_cycle = 65536((20000)/pulse_length) - 1
-
+    pca.channels[thruster].duty_cycle = int((13.107 * speed) + 4915.125)
 }
 
 
